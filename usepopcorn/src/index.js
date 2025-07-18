ece751bb1ec06c338775500490bbbca76a417f8d
ShellyKn <Shanellekn8@gmail.com>
2025-07-17 22:05:44 -0500

    Display final app, lesson completed

--- usepopcorn/src/index.js
+++ usepopcorn/src/index.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-// import './index.css';
-// import App from './App';
-import StarRating from './Star';
+import './index.css';
+import App from './App';
+// import StarRating from './Star';
+// import { useState } from 'react';
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// function Test() {
+//   const [movieRating, setMovieRating] = useState(0)
+//   return (
+//     <div>
+//       <StarRating maxRating={10} onSetRating={setMovieRating} />
+//       <div>movie was rated {movieRating}</div>
+//     </div>
+//   )
+// }
 root.render(
   <React.StrictMode>
-    {/* <App /> */}
-    <StarRating maxRating={5} />
-    <StarRating />
+    <App />
+    {/* <StarRating maxRating={5} />
+    <StarRating color="red" size={24} />
+    <Test /> */}
   </React.StrictMode>
 );
 
